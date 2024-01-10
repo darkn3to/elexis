1824b81c2d7ee874017402641d29906b2cbafbd6
Dhruv <62509177+darkn3to@users.noreply.github.com>
2024-01-10 10:01:07 +0530

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -8,8 +8,7 @@ contract Voting {
     Candidate[] public candidates;
     address owner;
     mapping(address => bool) public voters;
-    uint256 public votingStart;
-    uint256 public votingEnd;
+    uint256 public votingStart, votingEnd;
     constructor(string[] memory _candidateNames, uint256 _durationInMinutes) {
         for (uint256 i = 0; i < _candidateNames.length; i++) {
             candidates.push(
