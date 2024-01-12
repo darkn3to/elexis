8f32e5c8ec2bc0847775993588c1def05698aeda
Dhruv <62509177+darkn3to@users.noreply.github.com>
2024-01-12 12:22:40 +0530

    Update deploy.js
    Auto update the contract address in main.js.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 async function main() {
   const Voting = await ethers.getContractFactory("Voting");
-  const Voting_ = await Voting.deploy(["Lewis Hamilton", "Max Verstappen", "Charles Leclerc"], 10);
+  const Voting_ = await Voting.deploy(["Lewis Hamilton", "Max Verstappen", "Charles Leclerc", "Ayrton Senna", "Micheal Schumacher"], 10);
   console.log("Contract address:", Voting_.address);
+  const mainJsPath = path.join(__dirname, '..', 'main.js');
+  const mainJsContent = fs.readFileSync(mainJsPath, 'utf-8');
+  const updatedMainJsContent = mainJsContent.replace(/let contractAddress = "(.*)";/, `let contractAddress = "${Voting_.address}";`);
+  fs.writeFileSync(mainJsPath, updatedMainJsContent, 'utf-8');
 }
-
 main()
  .then(() => process.exit(0))
  .catch(error => {
