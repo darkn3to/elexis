ecd22395ecce2a420f5da8b2db31fea707ba9fae
Dhruv <62509177+darkn3to@users.noreply.github.com>
2024-01-12 12:27:08 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const path = require('path');
 async function main() {
   const Voting = await ethers.getContractFactory("Voting");
   const Voting_ = await Voting.deploy(["Lewis Hamilton", "Max Verstappen", "Charles Leclerc", "Ayrton Senna", "Micheal Schumacher"], 10);
-  console.log("Contract address:", Voting_.address);
+  console.log("Contract deployed. ");
   const mainJsPath = path.join(__dirname, '..', 'main.js');
   const mainJsContent = fs.readFileSync(mainJsPath, 'utf-8');
   const updatedMainJsContent = mainJsContent.replace(/let contractAddress = "(.*)";/, `let contractAddress = "${Voting_.address}";`);
